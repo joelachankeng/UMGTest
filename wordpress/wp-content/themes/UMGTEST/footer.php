63bb741e7ac8e43e3aa0f02d8766e5a138dc12c9
joelachankeng <47440462+joelachankeng@users.noreply.github.com>
2022-04-21 02:49:26 -0400

    developed footer

--- wordpress/wp-content/themes/UMGTEST/footer.php
+++ wordpress/wp-content/themes/UMGTEST/footer.php
@@ -8,12 +8,36 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
+    $custom_logo_id = get_theme_mod( 'custom_logo' );
+    $custom_logo_url = wp_get_attachment_image_src( $custom_logo_id , 'full' )[0];
 ?>
 
 
-<footer class="footer">
- 
-</footer>
-<?php wp_footer(); ?>
+    <footer class="footer">
+        <div class="container mx-auto">
+            <div class="copyright-column w-full">
+                <div class="logo">
+                        <a href="/">
+                            <img src="<?php echo $custom_logo_url;?>" alt="<?php echo get_bloginfo('name');?> " class="h-full w-full object-contain">
+                        </a>
+                </div>
+                <p class="copyright-text">
+                    © <?php echo date("Y"); ?> Joelparkin Achankeng, All Rights Reserved
+                </p>
+            </div>
+            <div class="social-menu-column w-full">
+                <?php
+                    wp_nav_menu( array( 
+                        'theme_location' => 'social-menu', 
+                        'container_class' => 'social-menu',
+                        'menu_class' => 'flex items-center ' 
+                        ) 
+                    ); 
+                ?>
+            </div>
+
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
